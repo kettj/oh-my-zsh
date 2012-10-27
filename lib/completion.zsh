11c30336fd1626af638dbd523db6bec8f5cb7287
fmw-jk <jannik.kett@googlemail.com>
2012-10-27 15:24:23 +0200

    complete .. to ../

--- lib/completion.zsh
+++ lib/completion.zsh
@@ -70,3 +70,6 @@ if [ "x$COMPLETION_WAITING_DOTS" = "xtrue" ]; then
   zle -N expand-or-complete-with-dots
   bindkey "^I" expand-or-complete-with-dots
 fi
+
+# provide .. as a completion
+zstyle ':completion:*' special-dirs ..
