cf24bfd4f17f9a6c7a10fbada34eb449c6a1e0ae
fmw-jk <jannik.kett@googlemail.com>
2012-12-09 11:45:22 +0100

    exported env var to '.zshenv'

--- custom/env.zsh
+++ custom/env.zsh
@@ -1,10 +1,1 @@
-source ~/.zsh_env
-
-export PYTHONPATH=/home/jannik/jenkins_DA/cob_jenkins_tools:$PYTHONPATH
-
-if [ -f /opt/ros/fuerte/setup.zsh ]; then
-    source /opt/ros/fuerte/setup.zsh
-fi
-export ROS_HOME=/home/jannik/git/ros
-export ROS_WORKSPACE=/home/jannik/git
-#export ROS_PACKAGE_PATH=/home/jannik/git/care-o-bot:/home/jannik/git/ros:$ROS_PACKAGE_PATH
+source ~/.zshenv
