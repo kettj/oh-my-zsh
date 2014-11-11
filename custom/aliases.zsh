e6032c7de35cef09d972e22ad56954be40fd57d2
fmw-jk <jannik.kett@googlemail.com>
2014-11-11 19:43:34 +0100

    load alias file depending on environment

--- custom/aliases.zsh
+++ custom/aliases.zsh
@@ -3,6 +3,12 @@
 if [ -f ~/.zsh_aliases_general ]; then
     source ~/.zsh_aliases_general
 fi
+if [ $(lsb_release -is) = "ManjaroLinux" ] && [ -f ~/.zsh_aliases_general_manjaro ]; then
+    source ~/.zsh_aliases_general_manjaro
+fi
+if [ $(lsb_release -is) = "Ubuntu" ] && [ -f ~/.zsh_aliases_general_ubuntu ]; then
+    source ~/.zsh_aliases_general_ubuntu
+fi
 
 # legacy
 if [ -f ~/.bash_aliases ]; then
