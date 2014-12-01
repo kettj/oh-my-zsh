df5061a1e39c63bcf56c504824616a1ceb5281d2
fmw-jk <jannik.kett@googlemail.com>
2014-12-01 17:39:17 +0100

    outsourced aliases sourcing

--- custom/aliases.zsh
+++ custom/aliases.zsh
@@ -1,16 +1,2 @@
 #!/usr/bin/env zsh
-
-if [ -f ~/.zsh_aliases_general ]; then
-    source ~/.zsh_aliases_general
-fi
-if [ $(lsb_release -is) = "ManjaroLinux" ] && [ -f ~/.zsh_aliases_general_manjaro ]; then
-    source ~/.zsh_aliases_general_manjaro
-fi
-if [ $(lsb_release -is) = "Ubuntu" ] && [ -f ~/.zsh_aliases_general_ubuntu ]; then
-    source ~/.zsh_aliases_general_ubuntu
-fi
-
-# legacy
-if [ -f ~/.bash_aliases ]; then
-    source ~/.bash_aliases
-fi
+source ~/.zsh_aliases
